d6ff8837d86cc7f3c52933b2e037747bb8c3ecee
keyarr <keyarzera@protonmail.com>
2026-08-19 11:34:56 -0300

    build(release): bump to 0.3.5 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "dev.degoogle"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.3.4"
+        versionCode = 8
+        versionName = "0.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
